451c97ee0298377c54015fc67f0d7ad3ed28f909
Hieu Do <vanhieu99it@gmail.com>
2023-09-22 10:19:46 +0700

    up Iterator example

---
+++ src/main/java/com/hido/somethingtosave/SomethingToSaveApplication.java
@@ -0,0 +1,13 @@
+package com.hido.somethingtosave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomethingToSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomethingToSaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hido/somethingtosave/SomethingToSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hido.somethingtosave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomethingToSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
