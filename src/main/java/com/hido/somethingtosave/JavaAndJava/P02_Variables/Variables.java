0020e118b936bdec14b177bdf2346394f079f806
Hieu Do <vanhieu99it@gmail.com>
2023-10-26 10:45:42 +0700

    add variables

---
+++ src/main/java/com/hido/somethingtosave/JavaAndJava/P02_Variables/Variables.java
@@ -0,0 +1,32 @@
+package com.hido.somethingtosave.JavaAndJava.P02_Variables;
+
+public class Variables {
+    int globalVariable; // Biến toàn cục
+
+    public void dataType(){
+        int localVariables; // Biến cục bộ
+
+        //Các kiểu dữ liệu nguyên thủy
+
+        //Kiểu số nguyên
+        byte bNumber = 127;
+        short sNumber = 32000;
+        int iNumber = 540000;
+        long lNumber = 450000L;
+
+        //Kiểu số thực
+        float fNumber = 5.7f;
+        double dNumber = 6.5d;
+        //or
+        double number = 6.5;
+
+        //kiểu kí tự
+        char ch1 = 'a';
+        char ch2 = '5';
+        char ch3 = 65; // ==A dựa vào bảng mã ASCII
+
+        //Kiểu luận lý
+        boolean isTrue = true;
+        boolean isFalse = false;
+    }
+}
