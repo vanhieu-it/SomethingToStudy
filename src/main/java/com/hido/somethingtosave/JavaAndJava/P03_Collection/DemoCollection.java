196c1db132d7c05d58a7c43ad20d16e89b350fcb
Hieu Do <vanhieu99it@gmail.com>
2023-10-27 11:05:29 +0700

    add HashSet and Sort demo

--- src/main/java/com/hido/somethingtosave/JavaAndJava/P03_Collection/DemoCollection.java
+++ src/main/java/com/hido/somethingtosave/JavaAndJava/P03_Collection/DemoCollection.java
@@ -2,7 +2,16 @@ package com.hido.somethingtosave.JavaAndJava.P03_Collection;
 
 public class DemoCollection {
     public static void main(String[] args) {
+
+        demoList();
+        demoHashSet();
+        demoSort();
+    }
+
+    static void demoList() {
+
         DemoList demoList = new DemoList();
+
         demoList.simpleForLoop();
 
         demoList.enhanceForLoop();
@@ -20,5 +29,14 @@ public class DemoCollection {
         demoList.forEachLoopB();
 
         demoList.streamForEach();
+    }
+
+    static void demoHashSet() {
+        DemoHashSet hs = new DemoHashSet();
+    }
+
+    static void demoSort() {
+        DemoSort s = new DemoSort();
+        s.sort();
     }
 }
